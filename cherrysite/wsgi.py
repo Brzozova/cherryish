be9f3d1160b0b4ea4c3270360f32d1e8a2b6980c
Brzozova <Brzozova@users.noreply.github.com>
2021-04-18 22:35:46 +0200

    Update with .env files

--- cherrysite/wsgi.py
+++ cherrysite/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cherrysite.settings')
-
+project_folder = os.path.expanduser('~/cherryish')
+load_dotenv(os.path.join(project_folder, '.env'))
 application = get_wsgi_application()
