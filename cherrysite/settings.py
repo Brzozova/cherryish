0e55b055dc3b8f62a0b107b978ab553f66d9b052
Brzozova <brzozowwwska@gmail.com>
2021-04-18 17:14:05 +0200

    Add python-dotenv to secure SECRET_KEY

--- cherrysite/settings.py
+++ cherrysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_socgd3qn^vp*31pl+$=8h#*1bwy=r39crw%xw3r_bv15s!%3!'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
